d278ce25b598e1cd9696e716d0c031264aef9ed7
Nicola Dellarocca <della90@msn.com>
2015-11-13 13:46:21 +0000

    Added bootstrap stuff

---
+++ scans/js/app.js
@@ -0,0 +1,88 @@
+function beforeAjax() {
+	// clear table data
+	$('#results thead').empty();
+	$('#results tbody').empty();
+
+	// Set loading message
+	$('#loading').text('Loading...');
+}
+
+$(function($) {
+
+    // Handle navbar pills click
+	$("#mainmenu > li > a").click(function(e) {
+        // Hide error message
+        if ($("#error").is(":visible")) {
+            $("#error").hide()
+        }
+        
+        // Show "loading" label
+        $('#loading').show()
+        
+        $('#placeholder').empty()
+        
+		// Get the family name
+        var text = $(this).text().toLowerCase();
+        
+        // Load data from the other page
+        $("#placeholder").load(text+".html", function(a, b, c){
+            // Will be executed AFTER loading done            
+            $("#lastUpdate").text(c.getResponseHeader("last-modified") || 'date not found')
+            
+            if (c.status != 200) {
+                $("#error").empty().text("Error "+c.status).show()
+            }
+            
+            // Hide "loading" label
+            $("#loading").hide()
+        });
+        
+        /*
+
+        // Perform AJAX request
+		$.ajax({
+			beforeSend: beforeAjax,
+			dataType: "json",
+			url: 'file_retriever.php',
+			data: {family: text}
+		})
+        // success
+		.done(function(data) {
+			$('#loading').empty();
+			var table = $("#results");
+			var lastUpdate = data.updated;
+
+			var date = new Date(lastUpdate*1000);
+
+			$('#lastUpdate').text(date);
+
+			var arrayData = data.data;
+			$.each(arrayData, function(i, item) {
+				if (i==0) {
+					var father = table.find('thead');
+				} else {
+					father = table.find('tbody');
+				}
+
+				var tr = $('<tr>');
+				$.each(item, function(j, jitem) {
+					tr.append($("<td>").text(jitem));
+				});
+
+				father.append(tr);
+				table.append(father);
+			});
+		})
+        // error
+		.fail(function(jqXHR, textStatus, errorThrown) {
+			console.log(jqXHR);
+			console.log(textStatus);
+			console.log(errorThrown);
+		});
+        
+        */
+	});
+
+	// load first pill data when the page is loaded
+	$("#mainmenu > li > a").first().click();
+});
