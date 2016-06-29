74353f8d47206e5e6c20495eddb8760a7ed81694
Nicola Dellarocca <della90@msn.com>
2016-06-29 13:06:32 +0000

    Added pages on Wed Jun 29 13:06:32 UTC 2016

--- scans/js/app.js
+++ scans/js/app.js
@@ -150,7 +150,7 @@ $(function($) {
 	// load first pill data when the page is loaded
 	$("#mainmenu > li > a").first().click();
      
-        $('#datatable').DataTables({
+        $('#datatable').DataTable({
             data: window.datatable_data
         });
 });
